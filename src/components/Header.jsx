713d7013619f3061e96ffab63cc607561f5bdbfd
Emily Su <emilyzqsu@gmail.com>
2024-06-16 22:33:33 -0400

    Updated Notebook link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
             </p>
             <h2>Results</h2>
             <p> From k-means clustering, we discovered 3 potential subtypes of Gas Giants: Type 1: Jupiter-like, Type 2, and Type 3. Gas Giant Type 2 exoplanets (and Gas giants overall) have higher capabilities to scatter bodies in their planetary system compared to other types based on their Safronov number. This can lead to the exoplanets ejecting planets out of their planetary system and result in rogue planets, which are hard to detect. A question to further delve into is what other properties of Gas Giant Type 2 exoplanets make them more capable of scattering.</p>
-            <p>The scatterplot below is an interactive form of the data visualization created from the original notebook exploring the data. The original notebook can be found <a href = "https://github.com/moonsdust/exoplanets/blob/main/Exoplanets_Notebook.ipynb" target = "_blank">here</a>. Curious about the technologies used for this dashboard? Go <a href ="https://github.com/moonsdust/exoplanets/tree/main" target = "_blank">here</a>.</p>
+            <p>The scatterplot below is an interactive form of the data visualization created from the original notebook exploring the data. The original notebook can be found <a href = "https://colab.research.google.com/drive/1GTdbPkYI2Dnu6MuWAgI0OFY3ufOpEH2P?usp=sharing" target = "_blank">here</a>. Curious about the technologies used for this dashboard? Go <a href ="https://github.com/moonsdust/exoplanets/tree/main" target = "_blank">here</a>.</p>
         </header>
     )
 }
